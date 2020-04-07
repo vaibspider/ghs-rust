7facde678dee292b6678674f0c5b094fbbdf412f
Vaibhav Kurhe <mcs192572@cse.iitd.ac.in>
2020-04-07 11:14:41 +0530

    Reading from input file

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use petgraph::graph::UnGraph;
+use std::io::{self, BufRead};
+fn main() {
+  let stdin = io::stdin();
+  for line in stdin.lock().lines() {
+    println!("{}", line.unwrap());
+  }
+}
